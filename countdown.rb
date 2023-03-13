05288a142174fc417d219042ca0f80a8e07dc2ba
jacinta mwangi <jacinta.mwangi@student@moringaschool.com>
2023-03-13 12:00:54 +0300

    Done

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
-#write your code here
+require 'pry'
 
-def countdown
+def countdown(num)
+  num == 10
+    while num > 0 do
+         puts "#{num} SECOND(S)!"
+         num = num -1
+     end
+        return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(num)
+    num == 10
+    while num > 0 do
+        sleep 5
+         puts "#{num} SECOND(S)!"
+         num = num -1
+     end
+        return "HAPPY NEW YEAR!"
 end
